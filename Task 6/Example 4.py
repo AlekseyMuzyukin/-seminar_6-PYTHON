df8dbb8a6e9614a32c0fccb33a37d84a2bb3bdee
Aleksey <J2k_pot@mail.ru>
2022-12-14 14:13:05 +0300

    add 4

---
+++ Task 6/Example 4.py
@@ -0,0 +1,4 @@
+lst =lst = list(map(int, input("Введите числа через пробел:\n").split()))
+temp = len(lst) // 2 + 1 if len(lst) % 2 != 0 else len(lst) // 2
+lst_2 = [lst[i] * lst[len(lst) - i - 1] for i in range(temp)]
+print(lst, lst_2)
